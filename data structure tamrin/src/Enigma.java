fa4ac402ed93e6d82572c415feedaa8593ac6e55
ali <alikhorasani2002@gmail.com>
2022-12-26 13:26:46 +0330

    add necessary hash maps

---
+++ data structure tamrin/src/Enigma.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Enigma
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        HashMap<String,String> plugBoardMap=new HashMap<>();
+        HashMap<String,String>  rotor1Map=new HashMap<>();
+        HashMap<String,String>  rotor2Map=new HashMap<>();
+        HashMap<String,String>  rotor3Map=new HashMap<>();
+        HashMap<String,String>  reflectorMap=new HashMap<>();
+    }
+}
